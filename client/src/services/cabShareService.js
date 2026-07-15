dabcf79b74865ab1d45410f6e5cf818246aeadee
nitin5117 <nitinmeenaa5117@gmail.com>
2026-07-15 13:46:20 +0530

    Connect frontend to Render backend

--- client/src/services/cabShareService.js
+++ client/src/services/cabShareService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 export const createCabShare = async (data) => {
   const response = await API.post("/cabshares", data);
