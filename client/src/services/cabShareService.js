3a6544d9ca03964d7e7b51e27e80662a8ff994a0
nitin5117 <nitinmeenaa5117@gmail.com>
2026-07-17 10:50:40 +0530

    Add Group size

--- client/src/services/cabShareService.js
+++ client/src/services/cabShareService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://daan-iitg.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 export const createCabShare = async (data) => {
   const response = await API.post("/cabshares", data);
